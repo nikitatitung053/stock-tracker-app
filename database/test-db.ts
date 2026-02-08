53e0fcaff27bb46e0f4c659868e2bcaec4e097b7
nikitatitung053 <nikitatitung@gmail.com>
2026-02-08 22:20:41 +0545

    implement database setup

---
+++ database/test-db.ts
@@ -0,0 +1,16 @@
+import { connectToDatabase } from "./mongoose";
+
+async function main(){
+    try{
+        await connectToDatabase();
+
+        console.log("OK: Database connection succeeded");
+        process.exit(0);
+    }catch(err){
+        console.error("Error:Database connection failed");
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+main();
